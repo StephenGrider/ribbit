e9b9181e4044629aa9f7dce18de698bbbf60d1b2
StephenGrider <ste.grider@gmail.com>
2014-02-19 20:44:56 -0800

    Initial Commit

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by stephen g on 2/19/14.
+//  Copyright (c) 2014 stephen g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
